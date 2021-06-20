06540b7c00672c93406c193b868d4d3992d3f0e5
Can Nahum <cannahum@outlook.com>
2021-06-19 17:21:57 -0700

    with react-backend integration and some styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Carousel } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
+  const baseUri: string = process.env.REACT_APP_API_URL!;
+
+  const [allPhotos, setAllPhotos] = useState([]);
+
+  async function fetchPhotos() {
+    const { data } = await axios.get(`${baseUri}getAllPhotos`);
+    console.log(data);
+    setAllPhotos(data);
+  }
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  function getCarouselImage(photo: any) {
+    return (
+      <Carousel.Item interval={1000} style={{ height: 350 }}>
+        <img src={photo.url} alt={photo.filename} className="h-100"/>
+        <Carousel.Caption>
+          <h3 style={{backgroundColor: 'rgba(0,0,0,0.3)'}}>{photo.filename}</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from React in CDK
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-secondary min-vh-100">
+      <h1 className="display-3 p-3 mb-5">Super Mario & Friends</h1>
+      <Carousel>{allPhotos.map(getCarouselImage)}</Carousel>
     </div>
   );
 }
